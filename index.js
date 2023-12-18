f902e6354726ec7fd5b2ce64fb8c1137d0cf3544
ronjohnson owuor <ronjohnsonowuor83@gmail.com>
2023-12-18 04:51:26 +0300

    finished the desktop version

--- index.js
+++ index.js
@@ -2,4 +2,8 @@
 var scroll_box = document.querySelector("#scrolls");
 window.addEventListener('scroll',()=>{
 	scroll_box.style.display="none";
-})
+});
+
+
+var button_chat = document.getElementById("chat_whatsapp");
+button_chat.addEventListener("click",()=>window.location.href="https://wa.me/254745097705");
